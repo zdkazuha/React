cc6858356acc23ac1eada947430e738dfaa07f06
zdkazuha <aartemchelyk@gmail.com>
2025-07-11 18:10:20 +0300

    Update

--- 00_Homework/04_Homework/vite-project/src/component/Card.jsx
+++ 00_Homework/04_Homework/vite-project/src/component/Card.jsx
@@ -28,15 +28,15 @@ function Card({src, productDescription , reviews, inStock, price, discount }) {
                 <div className="Top">
                     <img className="Image" src={src} alt="" />
                     <p onClick={AddHeart} className="Heart"><span className="heart">{heart}</span></p>
-                    <p className={discount === undefined ? "notDiscount" : "Discount"}>-{discount}%</p>
+                    <p className={discount === undefined || discount === 0 ? "notDiscount" : "Discount"}>-{discount}%</p>
                 </div>
 
                 <div className="Bottom">
                     <p>{productDescription}</p>
                     <p>💬 {reviews === 0 ? "Залишити відгук" : `Кількість відгуків ${reviews}`}</p>
                     <p color={inStock ? "green" : "red"}>{inStock ? "Є в наявності" : "Немає в наявності"}</p>
-                    <p className={discount === undefined ? "Price" : "OldPrice"} >{price} $</p>
-                    <p className={discount === undefined ? "notDiscount" : "NewPrice"}>{price - (price * discount / 100)} $</p>
+                    <p className={discount === undefined || discount === 0  ? "Price" : "OldPrice"} >{price} $</p>
+                    <p className={discount === undefined || discount === 0  ? "notDiscount" : "NewPrice"}>{price - (price * discount / 100)} $</p>
                 </div>
                     <p onClick={AddBoart} className="Boart">{icon}</p>
             </div>
